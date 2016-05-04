6805d32b56d618b0b31794a021b8939bee45c0d2
Luis De Siqueira <louthebrew@gmail.com>
2016-05-04 15:50:41 -0700

    updating for release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'LouTheBrew@gmail.com'
 license 'MIT'
 description 'Installs/Configures web things'
 long_description 'Installs/Configures web things'
-version '0.1.0'
+version '0.0.1'
 
 depends 'poise'
 depends 'poise-service'
