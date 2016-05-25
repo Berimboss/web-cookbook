5c2663d8b34d0496b9452f24313cfd5832e6e49c
Luis De Siqueira <louthebrew@gmail.com>
2016-05-24 21:47:26 -0700

    builtin essentials

--- publish.sh
+++ publish.sh
@@ -1,2 +1,2 @@
 ln -s ~/.chef/knife.rb.supermarket ~/.chef/knife.rb
-knife cookbook site share poise-web "Package Management" -o ~/.work/LouTheBrew/cookbooks/
+knife cookbook site share poise-web "Package Management" -o ~/.work/github.com/LouTheBrew/
